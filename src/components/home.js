585665e9d8bf43900f761937d293c231d0e7797f
half6 <christofferhalf6@gmail.com>
2023-04-25 13:45:35 +0200

    homePage

--- src/components/home.js
+++ src/components/home.js
@@ -1,31 +1,11 @@
 
 import data from "./data.js";
-
+const latestSection = document.querySelector('.latest-posts-section')
 
 
 function renderHTML(){
 
         // Hero section
-
-        const homeHeader = document.createElement('div')
-        homeHeader.className = 'home-header'
-        
-        const heroText = document.createElement('div')
-        heroText.className = 'hero-text'
-
-        const heroTitle = document.createElement('h1')
-        heroTitle.className = 'title-site title-home'
-        heroTitle.textContent = 'Welcome to WAVE'
-        const heroSubtitle = document.createElement('h2')
-        heroSubtitle.textContent = 'The number one electro blog'
-
-        heroText.append(heroTitle,heroSubtitle)
-        homeHeader.append(heroText)
-
-        const latestPosts = document.createElement('section')
-        const latestPostsTitle = document.createElement('h1')
-        latestPostsTitle.textContent = 'Latest posts'
-        latestPosts.append(latestPostsTitle)
 
         //Latest blog posts
 
@@ -33,22 +13,53 @@ function renderHTML(){
 
 
         const postContainer = document.createElement('div')
+        postContainer.className = 'blog-card'
 
+        const imgContainer = document.createElement('div')
+        imgContainer.className = 'blog-card-img-container'
         const postImage = document.createElement('img')
         postImage.src = post.img
+        postImage.className = 'blog-card-img'
+        imgContainer.append(postImage)
+
+        const cardBody = document.createElement('div')
+        cardBody.className = 'card-body'
+
+        const category = document.createElement('span')
+        category.className = `${post.category} tag`
+        category.textContent = `${post.category}`
     
-        const postTitle = document.createElement('h2')
+        const postTitle = document.createElement('a')
         postTitle.textContent = post.title
+        postTitle.className = 'title-link'
+        postTitle.href=`details.html?id=${post.id}`
     
         const postArticle = document.createElement('p')
         postArticle.textContent = post.subTitle
     
         const postArticleContainer = document.createElement('article')
         postArticleContainer.append(postTitle, postArticle)
-    
-        postContainer.append(postArticleContainer, postImage)
         
-        latestPostContainer.append(postContainer)
+        const cardBottom = document.createElement('div')
+        cardBottom.className = 'card-bottom'
+
+        const writer = document.createElement('div')
+        writer.className = 'writer'
+
+
+
+        const userInfo = document.createElement('div')
+        const userName = document.createElement('p')
+        userName.textContent = `Writer: ${post.writer}`
+        const dateWritten = document.createElement('small')
+        dateWritten.textContent = `Published: ${post.date}`
+        userInfo.append(userName,dateWritten)
+        writer.append( userInfo)
+        cardBottom.append(writer)
+        cardBody.append(category,postArticleContainer,cardBottom)
+        postContainer.append(imgContainer,cardBody)
+        
+        latestSection.append(postContainer)
         
     });
 
