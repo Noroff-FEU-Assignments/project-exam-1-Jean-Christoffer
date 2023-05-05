c2c7e0d8d707b6b4982c845a8abae74d84dae2d0
half6 <christofferhalf6@gmail.com>
2023-05-05 23:10:28 +0200

    Update details.js

--- src/components/details.js
+++ src/components/details.js
@@ -101,7 +101,7 @@ form.addEventListener('submit', async function(e) {
     
 })
 
-async function getComments(){
+/*async function getComments(){
     try{
         const API = new FetchHelper(`${import.meta.env.VITE_API_KEY2}`)
         const response = await API.get(`comments?post=${id}`)
@@ -113,14 +113,14 @@ async function getComments(){
     } catch(error){
         console.log(error)
     }
-}
+}*/
 
-function renderHtml(data, comments){
+function renderHtml(data){
 
     const formatedText = data.content.rendered
     const parser = new DOMParser();
     const nodes = parser.parseFromString(formatedText, 'text/html').body.childNodes
-
+    const comments = data._embedded.replies[0]
     
         nodes.forEach(element => {
             const paragraph = document.createElement('p')
@@ -189,9 +189,8 @@ async function renderPage(){
     try{
         loader.classList.add('show')
         const data = await getData(id)
-        const comments = await getComments()
-        console.log(comments)
-        renderHtml(data, comments)
+        //const comments = await getComments()
+        renderHtml(data)
     }catch(error){
         console.log(error)
     }finally{
