2d540644d77c379566df67e8b8928438dfe2d312
half6 <christofferhalf6@gmail.com>
2023-05-19 23:29:13 +0200

    sort newest/oldest almost there

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,212 +1,188 @@
 import FetchHelper from "./fetchHelper.js";
 import { gsap } from "gsap";
-const loader = document.querySelector('.spinner')
-const postSection = document.querySelector('.post-section')
-const category = document.querySelector('.category')
-const loadMore = document.querySelector('.load-more')
-const sortDate  = document.querySelector('.sort-date')
+const loader = document.querySelector(".spinner");
+const postSection = document.querySelector(".post-section");
+const category = document.querySelector(".category");
+const loadMore = document.querySelector(".load-more");
+const sortDate = document.querySelector(".sort-date");
 
-const searchForm = document.querySelector('.search-form')
-const searchInput = document.querySelector('#search')
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector("#search");
 
-let pages = 1
-let blogPosts = []
-let total = 0
-let categoriesArr = []
+let pages = 1;
+let blogPosts = [];
+let total = 0;
+let categoriesArr = [];
 
+async function getCategory() {
+  const API = new FetchHelper(`${import.meta.env.VITE_API_KEY}`);
+  const response = await API.get(`categories`);
+  const categories = await response.json();
+  categoriesArr.push(...categories);
 
-  async function getCategory(){
-    const API = new FetchHelper(`${import.meta.env.VITE_API_KEY}`)
-    const response = await API.get(`categories`)
-    const categories = await response.json()
-    categoriesArr.push(...categories)
-
-    return categoriesArr
-  }
-
-  getCategory()
-
-async function getData(categoryValue = '', searchQuery =''){
-    try{
-        const API = new FetchHelper(`${import.meta.env.VITE_API_KEY}posts`)
-        let response
-        if(searchQuery !== ''){
-          response  = await API.get(`?_embed&search=${searchQuery}`)
-        }
-        else {
-          response = await API.get(`?_embed${categoryValue ? categoryValue : ''}&orderby=date&order=desc&page=${pages}`)
-              
-        }       
-        const data = await response.json();
- 
-        total = response.headers.get('x-wp-totalpages');
-        return [data, total]
-    }catch(error){
-        console.log(error)
-    }
+  return categoriesArr;
 }
 
+getCategory();
 
+async function getData(categoryValue = "", searchQuery = "") {
+  try {
+    const API = new FetchHelper(`${import.meta.env.VITE_API_KEY}posts`);
+    let response;
+    if (searchQuery !== "") {
+      response = await API.get(`?_embed&search=${searchQuery}`);
+    } else {
+      response = await API.get(
+        `?_embed${
+          categoryValue ? categoryValue : ""
+        }&orderby=date&order=desc&page=${pages}`
+      );
+    }
+    const data = await response.json();
 
- function renderHTML(data, totalPosts,animate = true){
+    total = response.headers.get("x-wp-totalpages");
+    return [data, total];
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    blogPosts = data
-    const total = Number.parseInt(totalPosts,10)
-    console.log(pages, total)
+function renderHTML(data, totalPosts, animate = true) {
+  blogPosts = data;
+  const total = Number.parseInt(totalPosts, 10);
+  console.log(pages, total);
 
+  loadMore.addEventListener("click", async () => {
+    if (pages < total) {
+      pages += 1;
+      renderPage("", "", false);
+    }
+  });
 
-    loadMore.addEventListener('click', async () => {
-      if (pages < total) {
-        pages += 1;
-        renderPage('','',false)
+  if (pages === total || blogPosts.length === 0) {
+    loadMore.style.display = "none";
+  } else {
+    loadMore.style.display = "block";
+  }
+
+  if (blogPosts.length === 0) {
+    const noResults = document.createElement("h2");
+    noResults.textContent = `No results matching "${searchInput.value}" found`;
+    noResults.style.textAlign = "center";
+    postSection.append(noResults);
+  }
+
+  
+  blogPosts.forEach((post) => {
+    const formatedText = post.excerpt.rendered;
+    const parser = new DOMParser();
+    const formatedElement = parser.parseFromString(formatedText, "text/html")
+      .body.firstChild;
+    const formattedFinal = formatedElement.textContent;
+
+    const articleContainer = document.createElement("div");
+    articleContainer.className = "article-container";
+    articleContainer.id = post.id;
+
+    const imgContainer = document.createElement("div");
+    imgContainer.className = "posts-img-container";
+    const articleImg = document.createElement("img");
+    articleImg.src = `${post._embedded["wp:featuredmedia"][0].source_url}`;
+    articleImg.alt = `${post._embedded["wp:featuredmedia"][0].alt_text}`;
+    imgContainer.append(articleImg);
+
+    const titleContainer = document.createElement("div");
+    titleContainer.className = "article-title-container";
+
+    const postTitle = document.createElement("a");
+    postTitle.textContent = post.title.rendered;
+    postTitle.className = "title-link";
+    postTitle.href = `details.html?id=${post.id}`;
+
+    const postArticle = document.createElement("p");
+    postArticle.textContent = formattedFinal;
+    postArticle.className = "subTitle-text";
+
+    titleContainer.append(postTitle, postArticle);
+    articleContainer.append(imgContainer, titleContainer);
+    postSection.append(articleContainer);
+  });
+
+  if (animate) {
+    gsap.fromTo(
+      `.article-container`,
+      {
+        x: "200%",
+      },
+      {
+        x: 0,
+        duration: 0.5,
+        ease: "power2.out",
+        stagger: {
+          each: 0.15,
+          from: "start",
+        },
       }
-    });
-
-      if(pages === total || blogPosts.length === 0){
-        loadMore.style.display = 'none'
-      }else{
-        loadMore.style.display = 'block'
-      }
-
-      if(blogPosts.length === 0){
-        const noResults = document.createElement('h2')
-        noResults.textContent = `No results matching "${searchInput.value}" found`
-        noResults.style.textAlign = 'center'
-        postSection.append(noResults)
-       }   
-    blogPosts.forEach( post => {
-
-
-        const formatedText = post.excerpt.rendered
-        const parser = new DOMParser();
-        const formatedElement = parser.parseFromString(formatedText, 'text/html').body.firstChild;
-        const formattedFinal = formatedElement.textContent;
-
-        const articleContainer = document.createElement('div')
-        articleContainer.className = 'article-container'
-        articleContainer.id = post.id
-        
-        const imgContainer = document.createElement('div')
-        imgContainer.className = 'posts-img-container'
-        const articleImg = document.createElement('img')
-        articleImg.src = `${post._embedded['wp:featuredmedia'][0].source_url}`
-        articleImg.alt = `${post._embedded['wp:featuredmedia'][0].alt_text}`
-        imgContainer.append(articleImg)
-
-        const titleContainer = document.createElement('div')
-        titleContainer.className = 'article-title-container'
-
-        const postTitle = document.createElement('a')
-        postTitle.textContent = post.title.rendered
-        postTitle.className = 'title-link'
-        postTitle.href=`details.html?id=${post.id}`
-
-        const postArticle = document.createElement('p')
-        postArticle.textContent = formattedFinal
-        postArticle.className = 'subTitle-text'
-
-        titleContainer.append(postTitle,postArticle)
-        articleContainer.append(imgContainer, titleContainer)            
-        postSection.append(articleContainer)
-
-
-
-
-            });
-
- 
-
-
-            if(animate){
-
-              gsap.fromTo(`.article-container`,
-                {
-                x:'200%'
-                },
-            {
-              x:0,
-              duration:0.5,
-              ease: "power2.out",
-              stagger:{
-                each:0.15,
-                from:'start'
-              }
-            
-            })
-            }
-          
-
-          
-      
-
+    );
+  }
 }
 
 //filter
-category.addEventListener('change',()=>{
-      pages = 1
-    let found = categoriesArr.find(item => item.name === category.value)
-    found ? renderPage(`&categories=${found.id || ''}`,'') : renderPage('','')
-    postSection.textContent = ''
-
-})
-
-sortDate.addEventListener('change', () => {
-  let sortedValue
-
-  if(sortDate.value === 'oldest'){
-    sortedValue = blogPosts.slice().sort((a, b) =>  new Date(a.date) - new Date(b.date) );
-    
-  }
-  if(sortDate.value === 'newest'){
-    sortedValue= blogPosts.slice().sort((a, b) =>   new Date(b.date) - new Date(a.date) );
- 
-  }
-  renderHTML(sortedValue,'');
+category.addEventListener("change", () => {
+  pages = 1;
+  let found = categoriesArr.find((item) => item.name === category.value);
+  found ? renderPage(`&categories=${found.id || ""}`, "") : renderPage("", "");
+  postSection.textContent = "";
 });
 
 
+
 //search
-searchForm.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  search(searchInput.value)
-})
-function search(cleaner){
-    const trimmed = cleaner.trim()
-    const urlConvert = encodeURIComponent(trimmed);
-    postSection.textContent = ''
-    renderPage('',urlConvert)
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  search(searchInput.value);
+});
+function search(cleaner) {
+  pages = 1;
+  const trimmed = cleaner.trim();
+  const urlConvert = encodeURIComponent(trimmed);
+  postSection.textContent = "";
+  renderPage("", urlConvert);
 }
 
+async function renderPage(categoryValue = "", searchQuery = "", animate) {
+  try {
 
+    loader.classList.add("show");
+    const [data, totalPosts] = await getData(categoryValue, searchQuery);
 
-async function renderPage
-(
-  categoryValue = '', 
-  searchQuery = '', 
-  animate
-)
-  {
-    try{
-
-        loader.classList.add('show')
-        const [data, totalPosts ]= await getData(categoryValue,searchQuery) 
-        renderHTML(data,totalPosts,animate)
-
-    }catch(error){
-        console.log(error)
-    }finally{
-        loader.classList.remove('show')
-      
-    }
+    sortDate.addEventListener("change", () => {
+      let sortedValue;
+    
+      if (sortDate.value === "oldest") {
+        sortedValue = data
+          .slice()
+          .sort((a, b) => new Date(a.date) - new Date(b.date));
+      }
+      if (sortDate.value === "newest") {
+        sortedValue = data
+          .slice()
+          .sort((a, b) => new Date(b.date) - new Date(a.date));
+      }
+      postSection.textContent=''
+      renderHTML(sortedValue,totalPosts, true);
+    });
+    renderHTML(data, totalPosts, animate);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.classList.remove("show");
+  }
 }
 
-window.addEventListener('load',() => {
-  category.value = ''
-  sortDate.value = 'newest'
-})
+window.addEventListener("load", () => {
+  category.value = "";
+  sortDate.value = "newest";
+});
 
-
-
-
-
-renderPage()
+renderPage();
